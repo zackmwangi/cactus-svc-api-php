5b0606d8fb08016f9c80f24c12904ff4bb38173c
zackmwangi <zackmwangi@gmail.com>
2023-11-16 04:09:41 +0300

    adding structure

---
+++ app/settings/Settings.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Settings;
+
+use App\Settings\SettingsInterface;
+
+class Settings implements SettingsInterface
+{
+    private $settings;
+
+    public function __construct(array $settings)
+    {
+        $this->settings = $settings;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function get(string $key = '')
+    {
+        /*
+        echo 'Finding key ' . $key;
+        echo '----------------------------------------------------------------';
+        var_dump($this->settings);
+        echo '----------------------------------------------------------------';
+        */
+        return (empty($key)) ? $this->settings : $this->settings[$key];
+    }
+}
